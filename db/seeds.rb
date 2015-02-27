7cac2ae40517b6b38c7f2f371434ad133c9a71e3
bavaria95 <bavaria95@gmail.com>
2015-02-27 23:21:18 +0100

    added few cities to the seed file for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,8 @@
 user = CreateAdminService.new.call
 puts 'CREATED ADMIN USER: ' << user.email
+
+City.create(name: 'New York', short_name: 'NY')
+City.create(name: 'Berlin', short_name: 'BL')
+City.create(name: 'San Francisco', short_name: 'SF')
+City.create(name: 'Kiev', short_name: 'KV')
+City.create(name: 'Singapore', short_name: 'SG')
